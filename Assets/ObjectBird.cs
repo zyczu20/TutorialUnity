fabc98a2b7aafd20520aa4a3f0bab8b9a1c7c939
zyczu20 <czarny7771@vp.pl>
2023-04-08 22:59:20 +0200

    Implementing gravity

--- Assets/ObjectBird.cs
+++ Assets/ObjectBird.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ObjectBird : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float flapStrenght;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrenght;
+        }
     }
 }
+
+
+//https://www.youtube.com/watch?v=XtQMytORBmM&ab_channel=GameMaker%27sToolkit
+//16:09
